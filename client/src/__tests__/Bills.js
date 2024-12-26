c00fd63063910434f55b8fcabbcb1987f142b914
Pavol-69 <paul.valy@gmail.com>
2024-12-26 21:11:26 +0100

    Ajout tests unitaires/intégration pour Bills.js

--- client/src/__tests__/Bills.js
+++ client/src/__tests__/Bills.js
@@ -3,30 +3,15 @@
  */
 
 import "@testing-library/jest-dom";
-import {
-  screen,
-  waitFor,
-  fireEvent,
-  getByRole,
-  getByTestId,
-  getAllByTestId,
-} from "@testing-library/dom";
+import { screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import userEvent from "@testing-library/user-event";
 import router from "../app/Router.js";
-
-/*beforeAll(() => {
-  window.$ = jest.fn().mockImplementation(() => {
-    return {
-      modal: jest.fn(),
-      click: jest.fn(),
-      width: jest.fn(),
-    };
-  });
-});*/
+import Bills from "../containers/Bills.js";
+import mockStore from "../__mocks__/store.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -47,9 +32,10 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId("icon-window"));
       const windowIcon = screen.getByTestId("icon-window");
-      // Ajout except => Highlighted element = avec class "active-icon"
+      // Ajout de l'expect : si l'élément est highlighted, alors il a la class "active-icon"
       expect(windowIcon.className).toEqual("active-icon");
     });
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills });
       const dates = screen
@@ -63,22 +49,91 @@ describe("Given I am connected as an employee", () => {
     });
 
     /*Completion du test d'intégration pour augmenter le coverage*/
-    test("modalForm should appear when we click on the eye button", async () => {
+    test("modalForm should appear when we click on the eye button", () => {
+      // La fonction modal, venant de Bootstrap, ne peut pas être comprise par jest, qui n'a pas importé la bonne librairie
+      // L'astuce est donc de mock la fonction modal, en la remplaçant par une fonction qui fait exactement la même chose
+      $.fn.modal = jest.fn(() => {
+        screen.getByTestId("dialog").className += " show";
+      });
+
+      // Recréation du contexte
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
       const root = document.createElement("div");
-      //const user = userEvent.setup();
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
       window.onNavigate(ROUTES_PATH.Bills);
-      await waitFor(() => screen.getAllByTestId("icon-eye"));
-      const eyeBtn = screen.getAllByTestId("icon-eye")[0];
-      await waitFor(() => userEvent.click(eyeBtn));
 
-      const myModal = screen.getByTestId("dialog");
-      //await waitFor(() => myModal.toHaveClass("show"));
-      //expect(modal).toEqual("toto");
-      expect(myModal).toEqual("show");
-      //expect(screen.findByRole("dialog")).toHaveClass("show");
+      // Simulation d'un click sur le bouton avec l'icône d'oeil
+      userEvent.click(screen.getAllByTestId("icon-eye")[0]);
+
+      // La modal doit alors s'afficher, avec la class show
+      expect(screen.getByTestId("dialog")).toHaveClass("show");
+    });
+
+    test("should see NewBill page when we click on the newBill button", () => {
+      // Recréation du contexte
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+
+      // Simulation du click sur Nouvelle facture
+      userEvent.click(screen.getByTestId("btn-new-bill"));
+
+      // On devrait alors avoir à l'écran l'élément "form-new-bill"
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+    });
+
+    test("should get same bills with store.getBills & bills", async () => {
+      // Recréation du contexte
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      // On crée un élément Bills, qui devrait contenir la fonction getBills, que l'on souhaite tester ici
+      let myBills = new Bills({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage,
+      });
+
+      // Appel de la fonction getBills, et sauvegarde du résultat
+      myBills = await myBills.getBills().then((bills) => {
+        return bills;
+      });
+
+      // Chaque facture répertoriée par getBills devrait avoir son équivalent, avec le même ID, dans bills
+      // On parcourt donc tous les éléments de bills, et on voit si on trouve toutes les id dans myBills
+      let myBool = true;
+      bills.forEach((bill) => {
+        if (myBills.filter((myBill) => myBill.id == bill.id).length < 1) {
+          myBool = false;
+        }
+      });
+      expect(myBool).toEqual(true);
     });
   });
 });
--- client/src/__tests__/NewBill.js
+++ client/src/__tests__/NewBill.js
@@ -2,17 +2,75 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen, waitFor } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import mockStore from "../__mocks__/store.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import userEvent from "@testing-library/user-event";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import router from "../app/Router.js";
 
+jest.mock("../app/store", () => mockStore);
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+describe("Given I am connected as an employee and I am on NewBill Page", () => {
+  describe("When I upload a document", () => {
+    test("Then I should find the same fileUrl in the NewBill Object I created than the one I use as input data", async () => {
+      // Recréation du contexte
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "email@email.fr",
+        })
+      );
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      // Création d'un nouveau NewBill
+      let newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage,
+      });
+
+      // Simulation d'ajout d'un fichier dans l'input
+      // Problème => N'a aucun chemin ??
+      const file = new File(["toto"], "toto.jpg", {
+        type: "application/jpg",
+      });
+      userEvent.upload(screen.getByTestId("file"), file);
+
+      // On doit alors trouver le même fileUrl que celui présent dans mockStyore
+      const myResult = await mockStore.bills().create();
+      expect(newBill.fileUrl).toEqual(myResult.fileUrl);
+    });
+  });
+
+  describe("When I complete the NewFile panel and submit it", () => {
+    test("Then I should be on the Bills page", async () => {
+      // Recréation du contexte
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+    });
+  });
+});
